b5c0d886bd0757ea1b37e9af5175cc595875cd1c
cody_glb <goodmorning_glb@163.com>
2020-03-16 13:56:08 +0800

    add System.out.println("test restore");

--- src/main/java/com/okisz/git/Hello.java
+++ src/main/java/com/okisz/git/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("TestGit");
 		System.out.println("hot fix....");
+		System.out.println("test restore");
 	}
 
 }
